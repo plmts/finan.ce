bcbceb1600f65ffc305dc315f637bd27c2256f1f
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-07-17 16:11:26 -0300

    criada a url e view definir_planejamento. agora é trabalhar nela

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from perfil.models import Categoria
+from django.http import HttpResponse
 
 # Create your views here.
 def definir_planejamento(request):
-    pass
+  categorias = Categoria.objects.all()
+  return render(request, 'definir_planejamento.html', {'categorias': categorias})
+  
