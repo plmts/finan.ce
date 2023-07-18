7c2cb460b83c0ab1080eb1565343fde0d69a91a3
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-07-18 14:41:08 -0300

    finalizada url e view contas a pagar. registrada no admin para poder ser acessado via navegador.

--- contas/views.py
+++ contas/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from perfil.models import Categoria
-# Create your views here.
+from .models import ContaPagar
+from django.contrib.messages import  constants
+from django.contrib import messages
 
 def definir_contas(request):
     if request.method == "GET":
         categorias = Categoria.objects.all()
         return render(request, 'definir_contas.html', {'categorias': categorias})
+    else:
+        titulo = request.POST.get('titulo')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        valor = request.POST.get('valor')
+        dia_pagamento = request.POST.get('dia_pagamento')
+
+        conta = ContaPagar(
+                titulo = titulo,
+                categoria_id = categoria,
+                descricao = descricao,
+                valor = valor,
+                dia_pagamento = dia_pagamento,
+        )
+        conta.save()
+        messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso!')
+        return redirect('/contas/definir_contas')
